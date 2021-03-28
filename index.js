00483900bd636e4cfb47e83f2524264fe72c7b63
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-03-28 23:20:55 +0200

    feat: autologin

--- index.js
+++ index.js
@@ -40,17 +40,143 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var puppeteer_1 = __importDefault(require("puppeteer"));
+var fs_1 = __importDefault(require("fs"));
+var child_process_1 = __importDefault(require("child_process"));
+var credLocation = "./creds.json";
+if (!fs_1.default.existsSync(credLocation)) {
+    fs_1.default.writeFileSync(credLocation, "{\n    \"email\": \"[INSERT_EMAIL_HERE]\",\n    \"password\": \"[INSERT_PASSWORD_HERE]\"\n}");
+    child_process_1.default.execSync("start creds.json");
+    process.exit(0);
+}
+var meetLink = process.argv[2];
+var eIn = Number(process.argv[3]);
+if (Number.isNaN(eIn)) {
+    throw new Error("Vole ses kokot.");
+}
+var creds = JSON.parse(fs_1.default.readFileSync(credLocation).toString());
 function main() {
     return __awaiter(this, void 0, void 0, function () {
-        var browser;
+        var browser, page;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0: return [4 /*yield*/, puppeteer_1.default.launch({
-                        headless: false
+                        headless: false,
+                        args: ['--use-fake-ui-for-media-stream']
                     })];
                 case 1:
                     browser = _a.sent();
+                    return [4 /*yield*/, browser.newPage()];
+                case 2:
+                    page = _a.sent();
+                    return [4 /*yield*/, page.goto("https://accounts.google.com/servicelogin")];
+                case 3:
+                    _a.sent();
+                    return [4 /*yield*/, fillInput(page, "whsOnd zHQkBf", creds.email)];
+                case 4:
+                    _a.sent();
+                    return [4 /*yield*/, clickButton(page, "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qIypjc TrZEUc lw1w4b")];
+                case 5:
+                    _a.sent();
+                    return [4 /*yield*/, waitForRequests(page, 7)];
+                case 6:
+                    _a.sent();
+                    return [4 /*yield*/, wait(200)];
+                case 7:
+                    _a.sent();
+                    return [4 /*yield*/, fillInput(page, "whsOnd zHQkBf", creds.password)];
+                case 8:
+                    _a.sent();
+                    return [4 /*yield*/, clickButton(page, "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qIypjc TrZEUc lw1w4b")];
+                case 9:
+                    _a.sent();
+                    return [4 /*yield*/, waitForRequests(page, 30)];
+                case 10:
+                    _a.sent();
+                    return [4 /*yield*/, wait(200)];
+                case 11:
+                    _a.sent();
+                    return [4 /*yield*/, page.goto(meetLink)];
+                case 12:
+                    _a.sent();
+                    return [4 /*yield*/, clickButton(page, "U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb")];
+                case 13:
+                    _a.sent();
+                    return [4 /*yield*/, clickButton(page, "U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb")];
+                case 14:
+                    _a.sent();
+                    return [4 /*yield*/, wait(500)];
+                case 15:
+                    _a.sent();
+                    return [4 /*yield*/, clickButton(page, "uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")];
+                case 16:
+                    _a.sent();
+                    return [4 /*yield*/, wait(eIn)];
+                case 17:
+                    _a.sent();
+                    return [4 /*yield*/, browser.close()];
+                case 18:
+                    _a.sent();
                     return [2 /*return*/];
+            }
+        });
+    });
+}
+function fillInput(page, cls, text) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, page.evaluate(function (cls, text) {
+                        var arr = document.getElementsByClassName(cls);
+                        for (var i = 0; i < arr.length; i++) {
+                            arr[i].value = text;
+                        }
+                    }, cls, text)];
+                case 1: return [2 /*return*/, _a.sent()];
+            }
+        });
+    });
+}
+function clickButton(page, cls) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, page.evaluate(function (cls) {
+                        var arr = document.getElementsByClassName(cls);
+                        for (var i = 0; i < arr.length; i++) {
+                            arr[i].click();
+                        }
+                    }, cls)];
+                case 1: return [2 /*return*/, _a.sent()];
+            }
+        });
+    });
+}
+function wait(ms) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, ms);
+    });
+}
+function waitForRequests(page, n, check) {
+    if (check === void 0) { check = function () { return true; }; }
+    return __awaiter(this, void 0, void 0, function () {
+        var i;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    i = 0;
+                    _a.label = 1;
+                case 1:
+                    if (!(i < n)) return [3 /*break*/, 4];
+                    return [4 /*yield*/, page.waitForRequest(check)];
+                case 2:
+                    _a.sent();
+                    _a.label = 3;
+                case 3:
+                    i++;
+                    return [3 /*break*/, 1];
+                case 4: return [2 /*return*/];
             }
         });
     });
--- index.ts
+++ index.ts
@@ -1,14 +1,73 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
+import childProcess from 'child_process';
+
+
+const credLocation = "./creds.json";
+
+if (!fs.existsSync(credLocation)) {
+    fs.writeFileSync(credLocation, `{\n    "email": "[INSERT_EMAIL_HERE]",\n    "password": "[INSERT_PASSWORD_HERE]"\n}`);
+    childProcess.execSync("start creds.json")
+    process.exit(0);
+}
+const meetLink = process.argv[2];
+const eIn = Number(process.argv[3]);
+
+if (Number.isNaN(eIn)) {
+    throw new Error("Vole ses kokot.");
+}
+
+const creds: {email: string, password: string} = JSON.parse(fs.readFileSync(credLocation).toString());
 
 async function main() {
     const browser = await puppeteer.launch({
-        headless: false
+        headless: false,
+        args: [ '--use-fake-ui-for-media-stream' ]
     });
     const page = await browser.newPage();
 
     await page.goto("https://accounts.google.com/servicelogin");
 
+    await fillInput(page, "whsOnd zHQkBf", creds.email);
+    await clickButton(page, "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qIypjc TrZEUc lw1w4b");
 
+    await waitForRequests(page, 7);
+    await wait(200);
+
+    await fillInput(page, "whsOnd zHQkBf", creds.password);
+    await clickButton(page, "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qIypjc TrZEUc lw1w4b");
+
+    await waitForRequests(page, 30);
+    await wait(200);
+
+    await page.goto(meetLink);
+    
+    await clickButton(page, "U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb");
+    await clickButton(page, "U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb");
+
+    await wait(500);
+
+    await clickButton(page, "uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt");
+
+    await wait(eIn);
+
+    await browser.close();
+}
+async function fillInput(page: puppeteer.Page, cls: string, text: string): Promise<void> {
+    return await page.evaluate((cls: string, text: string) => {
+        const arr = document.getElementsByClassName(cls) as HTMLCollectionOf<HTMLInputElement>;
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].value = text;
+        }
+    }, cls, text);
+}
+async function clickButton(page: puppeteer.Page, cls: string): Promise<void> {
+    return await page.evaluate((cls: string) => {
+        const arr = document.getElementsByClassName(cls) as HTMLCollectionOf<HTMLButtonElement>;
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].click();
+        }
+    }, cls);
 }
 function wait(ms: number): Promise<void> {
     return new Promise<void>((resolve, reject) => {
@@ -16,5 +75,10 @@ function wait(ms: number): Promise<void> {
             resolve();
         }, ms);
     })
+}
+async function waitForRequests(page: puppeteer.Page ,n: number, check: () => boolean = () => true): Promise<void> {
+    for (var i = 0; i < n; i++) {
+        await page.waitForRequest(check);
+    }
 }
 main();
